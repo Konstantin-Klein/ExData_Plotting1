7a2795849deff8c3d2dacd58d48869cda5ae3598
konstantin-klein <konstantin.klein@ensoltech.ru>
2015-01-08 23:21:19 +0300

    Plot 2

--- plot2.R
+++ plot2.R
@@ -14,10 +14,10 @@
 
 # * Add the PNG file and R code file to your git repository
 
-### Plot 1
+### Plot 2
 
 
-# Histogram of Global Active Power
+# Time Fluctuations of Global Active Power
 
 
 # ------------------------------------------------------------------
@@ -48,16 +48,24 @@ data <- read.table("../raw/household_power_consumption.txt",
 print(noquote("Filtering data by range of dates"))
 data <- data[data$Date %in% c("1/2/2007","2/2/2007"), ]
 
-# building a new plot to a PNG device
-print(noquote("Building a new plot to a PNG device"))
-png(file = "plot1.png", width = 480, 
-    height = 480, units = "px", 
-    pointsize = 12, 
-    bg = "transparent")
-hist(data$Global_active_power, 
-    main = "Global Active Power", 
-    col = "Red", 
-    xlab = "Global Active Power (kilowatts)")
+# build timeline for the data
+data$date_time <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S") 
+
+
+# building a new plot
+print(noquote("Building a new plot"))
+if (dev.cur() !=2) dev.new()  # platform dependent. Used for RStudio. 
+par(bg = "transparent")
+plot(x = data$date_time, y = data$Global_active_power, 
+     type = "l", main = "", 
+     ylab = "Global Active Power (kilowatts)",
+     xlab = "") 
+
+print(noquote("Copying the plot to a PNG device"))
+dev.copy(png, file = "plot2.png", width = 480, 
+      height = 480, units = "px", 
+      pointsize = 12, 
+      bg = "transparent")
 
 # closing off
 print(noquote("Closing the file"))
